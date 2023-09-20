3299157c641dfe58e57dc1811cf7ff858f6dff5d
Татьяна Хонякина <hanakinat@gmail.com>
2023-09-20 17:47:21 +0300

    Решение и условие третьей задачи

---
+++ task_3/task_3.js
@@ -0,0 +1,44 @@
+'use strict';
+
+// свой метод map
+
+let arr = [ 1, 5, 7, 2, 9 ];
+
+Array.prototype.myMap = function (callback) {
+    let newArray = [];
+
+    for (let i = 0; i < this.length; i++) {
+        newArray.push(callback(this[i], i));
+    }
+
+    return newArray;
+}
+
+let result = arr.myMap((item, index) => {
+    return {
+        elem: item,
+        index: index
+    }
+});
+
+// console.log(result);
+
+// свой метод filter
+
+Array.prototype.myFilter = function (callback) {
+    let newArray = [];
+
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i)) {
+            newArray.push(this[i]);
+        }
+    }
+
+    return newArray;
+}
+
+let resul = arr.myFilter((item, index) => {
+    return item > 2;
+});
+
+// console.log(resul)
