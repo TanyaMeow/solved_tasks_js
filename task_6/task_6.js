140f8000e1762e912203dc5b8e88c11a7f194f13
Татьяна Хонякина <hanakinat@gmail.com>
2023-09-21 12:46:26 +0300

    Удалены проверки

--- task_6/task_6.js
+++ task_6/task_6.js
@@ -2,7 +2,7 @@
 
 class MySet {
     constructor(array) {
-        if (!Array.isArray(array)) { throw 'Переданное значение не поддерживается' }
+        if (!Array.isArray(array)) { throw 'Переданное значение не поддерживается!' }
 
         this.array = array;
         this.size = this.array.length;
@@ -34,16 +34,3 @@ class MySet {
         this.size = this.array.length;
     }
 }
-
-let arr = new MySet([6, 8, 3, 9, 1]);
-
-console.log(arr);
-console.log(arr.size);
-console.log(arr.has(7))
-arr.add(7);
-console.log(arr);
-console.log(arr.has(7))
-console.log(arr.size);
-arr.delete(6);
-console.log(arr);
-arr.clear();
