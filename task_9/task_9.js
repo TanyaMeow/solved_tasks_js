a6aac858176810e5cae60df275f627c0c19e329e
Татьяна Хонякина <hanakinat@gmail.com>
2023-09-25 09:59:24 +0300

    Решение и условие девятой задачи

---
+++ task_9/task_9.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// Использование функции из задачи 8
+
+function asyncTimeout(timeout) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, timeout)
+    });
+}
+
+async function promiseStack(value) {
+    for (const func of value) {
+        await func();
+    }
+}
+
+promiseStack([
+    () => asyncTimeout(4000).then(() => console.log(1)),
+    () => asyncTimeout(2000).then(() => console.log(2)),
+    () => asyncTimeout(1000).then(() => console.log(3)),
+    () => asyncTimeout(3000).then(() => console.log(4)),
+]).then();
