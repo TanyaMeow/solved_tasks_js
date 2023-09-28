26efb50c59efaee4f7fd55b45dabcd288867129f
Татьяна Хонякина <hanakinat@gmail.com>
2023-09-28 09:09:37 +0300

    Решение и условие первой задачи

---
+++ DOM/task_1/task_1.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const form = document.getElementById('form1');
+const firstName = form.elements.fname;
+const lastName = form.elements.lname;
+const userData = document.querySelector('.data');
+
+function getFormvalue(event) {
+    event.preventDefault();
+
+    userData.textContent = firstName.value + ` ${lastName.value}`;
+}
+
+form.onsubmit = getFormvalue;
