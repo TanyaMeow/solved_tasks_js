dd7021f27e532b0d70e062710dccd84941203447
Татьяна Хонякина <hanakinat@gmail.com>
2023-09-28 09:11:16 +0300

    Решение и условие третьей задачи

---
+++ DOM/task_3/task_3.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const calculate = document.querySelector('[value="Calculate"]');
+
+function calculationSphereVolume(event) {
+    event.preventDefault();
+
+    const radius = document.querySelector('.radius').value;
+    const volume = document.querySelector('.volume');
+
+    volume.value = 4/3 * Math.PI * Math.pow(radius, 3);
+}
+
+calculate.addEventListener('click', calculationSphereVolume);
