8db85be9cb5dbfe7295141a82526dd264e4808c6
Татьяна Хонякина <hanakinat@gmail.com>
2023-09-29 15:03:18 +0300

    Исправлено замечание

--- DOM/task_2/task_2.js
+++ DOM/task_2/task_2.js
@@ -1,18 +1,22 @@
 // FIXME сделай так, что бы вторая таблица тоже поддерживала этот функционал.
-//  See task_2.html. Посыл замечания в том, что insertRow должен быть переиспользуемый
-const table = document.getElementById('sampleTable');
-const input = document.querySelector('input');
-const rows = document.getElementsByTagName('tr');
+//  See task_2.html. Посыл замечания в том, что insertRow должен быть переиспользуемый (DONE)
 
-function insertRow() {
+function insertRow(event) {
+    const table = event.target.parentElement.querySelector('table');
+    const rows = table.querySelectorAll('tr');
     const row = document.createElement('tr');
     const cell = document.createElement('td');
     const cell2 = document.createElement('td');
 
-    table.appendChild(row);
+    table
+        .appendChild(row);
 
-    row.appendChild(cell).innerHTML = `Row${rows.length} cell1`;
-    row.appendChild(cell2).innerHTML = `Row${rows.length} cell2`;
+    row.appendChild(cell)
+        .innerHTML = `Row${rows.length + 1} cell1`;
+    row.appendChild(cell2)
+        .innerHTML = `Row${rows.length + 1} cell2`;
 }
 
-input.onclick = insertRow;
+document
+    .querySelectorAll('input')
+    .forEach(inp => inp.addEventListener('click', insertRow));
